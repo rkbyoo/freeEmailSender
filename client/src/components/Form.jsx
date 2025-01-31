0e31408fb94f540a5a1d8cea819bdaa7f1b6e63c
rakib hussain <rakibhussain8812@gmail.com>
2025-01-31 14:30:00 +0530

    changed it for deploying

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -34,7 +34,7 @@ function Form() {
     console.log(formDataToSend)
 
     try {
-      const response = await fetch("http://localhost:4000/api/v1/emailsend", {
+      const response = await fetch(process.env.REACT_APP_BASE_URL, {
         method: "POST",
         body: formDataToSend,
       });
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const cors=require("cors")
 
 //middleware
 app.use(cors({
-    origin: 'http://localhost:3000', // Specify the frontend origin
+    origin: 'https://anon-email-sender-virid.vercel.app/', // Specify the frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
     credentials: true, // Enable cookies if needed
   }));
